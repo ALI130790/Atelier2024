022fe490c13449f0f47e220d79ae4e18058a258b
Alicia Alderete <aliciavanesa9@gmail.com>
2024-01-29 20:38:36 -0300

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <section>
+        <h1>
+          Bienvenidos al lugar donde sus sueños se hacen realidad!
+        </h1>
+        <h2>
+          En nuestro Atelier realizamos vestidos a medida para Novias, 15 años, Madrinas, Egresos, Invitadas, Cortejo de niñas, y vestidos en general.
+        </h2>
+        <h3>
+          Consultanos por presupuestos y pruebas de vestidos, te ofrecemos la mejor atencion.
+        </h3>
+      </section>
+    </>
+  )
+}
+
+export default App
